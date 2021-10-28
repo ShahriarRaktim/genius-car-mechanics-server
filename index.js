69cef2e12fc03b501f6267f1a26842976b04381b
ShahriarRaktim <shahriarahmedraktim@gmail.com>
2021-10-28 09:56:23 +0600

    added port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ObjectId= require('mongodb').ObjectId;
 require('dotenv').config()
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
@@ -58,6 +58,10 @@ async function run(){
     }
 }
 run().catch(console.dir);
+
+app.get('/', (req, res) =>{
+    res.send('Running Genius Server')
+});
 
 app.listen(port, ()=>{
     console.log("Running Genius Server on port",port)
